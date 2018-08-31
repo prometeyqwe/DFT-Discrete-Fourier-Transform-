eb0b8aa3fdda1899cb0285aa7567567350ee3449
prometeyqwe <kirilllov.evgeny@mail.ru>
2018-09-01 00:30:50 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cmath import exp, pi
 import matplotlib.pyplot as plt
+import numpy as np
 #--------- DFT -------------
 def DFT(input_arr):
     N = len(input_arr)
@@ -15,30 +16,28 @@ def DFT(input_arr):
 #--------- /DFT -------------
 
 # ------ reading from file------
-input_file = open('input_data.txt', 'r')
+input_file = open('input.txt', 'r')
 input_str = input_file.read()
 input_file.close()
 input_arr = input_str.split("\t")
-# print(input_arr)
 # ------ /reading from file------
 
-
 # ------ using DFT ------
-dl = 5
+T = float(input_arr[0])#read T
+N = len(input_arr)-1
+input_arr=input_arr[1:N]
 output = DFT(input_arr)
-N = len(output)
+N = len(input_arr)
 output_abs = [abs(i) for i in output]
 
-# print output_abs
 
-plotx = [i/dl - N/dl/2 for i in range(N)]
+plotx = np.linspace(0.0, 1/T,N)
 
-plt.plot(plotx, input_arr)
+plt.plot(plotx, output)
 plt.grid()
 plt.show()
 
 plt.plot(plotx, output_abs)
 plt.grid()
 plt.show()
-
 # ------ using DFT ------
